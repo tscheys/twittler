846c353d816cdae32948d133a0b478f333f8be2b
Scheysie <tim.scheys@gmail.com>
2015-07-07 16:33:15 +0200

    twitler now shows new tweets (most recent first)

---
+++ main.js
@@ -0,0 +1,37 @@
+      $(document).ready(function(){
+        var $body = $('body');
+        var $newTweetsButton = $('<button id="newTweets">Show new Tweets</button>');
+
+        var index = streams.home.length - 1;
+        var startTweets = 10;
+
+        $body.html('');
+        $newTweetsButton.appendTo($body);
+
+
+        while(index >= 0){
+          var $tweet = $('<div></div>');
+          var tweet = streams.home[index];
+          $tweet.text('@' + tweet.user + ': ' + tweet.message);
+          $tweet.appendTo($body);
+          index -= 1;
+        }
+
+        $("#newTweets").on("click", function () {
+
+          var totalTweets = streams.home.length - 1;
+
+          var newTweets = [];
+
+          for(var i = totalTweets; startTweets < i; --i ) {
+              var tweet = streams.home[i];
+              newTweets.push("<div>" + "@" + tweet.user + ":" + tweet.message + "time " + tweet.created_at +"</div>");
+
+          }
+          startTweets = totalTweets;
+        
+          var $newtwe = $(newTweets.join(''));
+          $newtwe.prependTo($body);
+         });
+
+      });
