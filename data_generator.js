a475b7b3e2d6eb5a7f4c6c455bd361605901951d
Scheysie <tim.scheys@gmail.com>
2015-07-09 10:42:41 +0200

    twittler now able to view other users' feed

--- data_generator.js
+++ data_generator.js
@@ -42,7 +42,7 @@ var generateRandomTweet = function(){
   var tweet = {};
   tweet.user = randomElement(users);
   tweet.message = randomMessage();
-  tweet.created_at = new Date();
+  tweet.created_at = moment();
   addTweet(tweet);
 };
 
@@ -52,7 +52,7 @@ for(var i = 0; i < 10; i++){
 
 var scheduleNextTweet = function(){
   generateRandomTweet();
-  setTimeout(scheduleNextTweet, Math.random() * 8000);
+  setTimeout(scheduleNextTweet, Math.random() * 1500);
 };
 scheduleNextTweet();
 
